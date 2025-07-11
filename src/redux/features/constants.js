135264353146c507457161124848f030b0e70bcd
sachzindu <133384344+sachzindu@users.noreply.github.com>
2025-07-11 22:07:58 +0530

    Update constants.js

--- src/redux/features/constants.js
+++ src/redux/features/constants.js
@@ -1,6 +1,6 @@
 export const BASE_URL = "https://itp-lms.onrender.com";
-export const USERS_URL = "https://itp-lms.onrender.com/api/users";
-export const CLASSES_URL = "https://itp-lms.onrender.com/api/course";
-export const LECS_URL="https://itp-lms.onrender.com/api/lecs"
-export const ASSIGNMENT_URL="https://itp-lms.onrender.com/api/assignments"
-export const SUPPORT_URL="https://itp-lms.onrender.com/api/ticket"
+export const USERS_URL = "/api/users";
+export const CLASSES_URL = "/api/course";
+export const LECS_URL="/api/lecs"
+export const ASSIGNMENT_URL="/api/assignments"
+export const SUPPORT_URL="/api/ticket"
